2445ee1f7bf5ce5061c5b71b932bd3446de8c630
NadaGhurmallah <165708616+NadaGhurmallah@users.noreply.github.com>
2024-10-18 20:52:16 +0300

    Update urls.py
    added paths for add and delete book

--- IS424_Project/Library/urls.py
+++ IS424_Project/Library/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
-     path('register/', views.register_view, name='register'),
+    path('register/', views.register_view, name='register'),
+    path('add/', views.addBook, name='add'),
+    path('delete', views.deleteBook, name='delete'),
 ]
