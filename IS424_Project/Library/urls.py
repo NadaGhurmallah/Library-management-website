a6e4056e833ead329ec1ee693ae8e31181bf5f1a
Reemasaud <443200537@student.ksu.edu.sa>
2024-10-28 20:14:52 +0300

    Update urls.py

--- IS424_Project/Library/urls.py
+++ IS424_Project/Library/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+appName= 'library'
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
@@ -8,6 +9,6 @@ urlpatterns = [
     path('add/', views.addBook, name='add'),
     path('delete', views.deleteBook, name='delete'),
     path('allBooks/', views.allBooks, name='allBooks'),
-    path('book/<int:book_id>/', views.specificBook, name='specificBook'),
+    path('specificBook/<int:book_id>/', views.specificBook, name='specificBook'),
     # Add other URL patterns as needed
 ]
