a7f5bbcb6d3eda5869fb8232ec6ea18bbf1f75c4
joudkhaild <151688443+joudkhaild@users.noreply.github.com>
2024-11-12 17:57:24 +0300

    Update urls.py

--- IS424_Project/Library/urls.py
+++ IS424_Project/Library/urls.py
@@ -5,11 +5,13 @@ app_name = "Library"
 
 urlpatterns = [
     path('menu/<str:username>/', views.menu, name='menu'),
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('add/<str:username>/', views.addbook, name='addbook'),
     path('deleteBooks/<str:username>/<int:book_id>/', views.deleteBook, name='deleteBook'), 
     path('allBooks/<str:username>/', views.all_books, name='all_books'),
     path('specificBook/<str:username>/<int:book_id>/', views.specificBook, name='specificBook'),
     path('update_book/<str:username>/<int:book_id>/', views.update_book, name='update_book'), 
+    path('logout/',views.logout , name='logout')
 ]
+
